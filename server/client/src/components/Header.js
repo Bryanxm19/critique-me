a14efcc5697a0e91af68bed6a21435fc0415a6e5
Bryan Mateer <bryanxmateer@gmail.com>
2017-09-30 23:35:37 -0400

    create header logic

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <li><a href="/auth/google">Login with Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>
+    }
+  }
+
   render() {
     return (
       <nav>
@@ -9,7 +22,7 @@ class Header extends Component {
             CritiqueMe
           </a>
           <ul className="right">
-            <li><a>Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -17,4 +30,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
