81a6f8a50fb1390945a9c8d131aca1149214bbf6
Bryan Mateer <bryanxmateer@gmail.com>
2017-09-30 20:54:20 -0400

    create fetchUser action

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,6 +2,5 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 
-const fetchUser = () => {
-  axios.get('/api/current_user');
-}
+export const fetchUser = () => async dispatch =>
+  dispatch({ type: FETCH_USER, payload: await axios.get('/api/current_user') });
